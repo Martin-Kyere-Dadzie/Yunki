0c8b96d69e96847a77c5dc7d5f6b570a7ff4c0d1
martin-kyere <abeikudadzie5@gmail.com>
2022-11-13 03:47:03 +0000

    user authentication

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,8 +2,32 @@
 import './SignIn.css';
 import yunkiRedLogo from './public/icons/yunki-red.svg';
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from './firebase';
+import { useState } from 'react';
 
 function SignIn () {
+
+    const auth = getAuth(app);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const createAccount = () => {
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            alert('You successfully created your Yunki account');
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            // const errorMessage = error.message;
+            console.log(errorCode);
+          });     
+    };
 
   return (
     <div className="Login__Section">
@@ -16,13 +40,13 @@ function SignIn () {
                 <h3>Sign in for Yunki to be the first to see inspiring content, news and exclusive offers.</h3>
             </div>
             <form className='Login__form' >
-                <label>Name:</label>
-                <input type={'text'} placeholder='Enter User Name.' required></input>
+                {/* <label>Name:</label>
+                <input type={'text'} placeholder='Enter User Name.' required onChange={(e) => setName(e.target.value)}></input> */}
                 <label>Email Address:</label>
-                <input type={'email'} placeholder='Enter User Email.' required></input>
+                <input type={'email'} placeholder='Enter User Email.' required onChange={(e) => setEmail(e.target.value)}></input>
                 <label>Password:</label>
-                <input type={'password'} placeholder='Enter User Password.' required></input>
-                <button type='submit' className='form__btn'>create your yunki account</button>
+                <input type={'password'} placeholder='Enter User Password.' required onChange={(e) => setPassword(e.target.value)}></input>
+                <button onClick={createAccount} type='submit' className='form__btn'>create your yunki account</button>
             </form>
             <div className='user__login'>Already have an account? <span><Link to='/login' className='User__Account'>Log In</Link></span></div>
             <div className='login__footer'>
